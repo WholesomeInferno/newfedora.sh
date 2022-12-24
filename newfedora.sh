d67e6e6bc43cfa02fd0f85c78cdccb8e945a74d7
Inferno <infernodev00@gmail.com>
2022-12-24 12:01:41 +0100

    Update #4
    Added github desktop

--- newfedora.sh
+++ newfedora.sh
@@ -12,6 +12,11 @@ sudo dnf install alacritty -y
 sudo dnf config-manager --add-repo https://repo.vivaldi.com/archive/vivaldi-fedora.repo
 sudo dnf install vivaldi-stable -y
 
+# install github desktop
+sudo rpm --import https://packagecloud.io/shiftkey/desktop/gpgkey
+sudo sh -c 'echo -e "[shiftkey]\nname=GitHub Desktop\nbaseurl=https://packagecloud.io/shiftkey/desktop/el/7/\$basearch\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://packagecloud.io/shiftkey/desktop/gpgkey" > /etc/yum.repos.d/shiftkey-desktop.repo'
+sudo dnf install github-desktop -y
+
 # install vscode
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
